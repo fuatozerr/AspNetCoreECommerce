bcfbe0e856015aaf7a38d7345e4d4e9ca7b2ead6
fuat özer <fuatozerr23@gmail.com>
2019-08-07 23:21:37 +0300

    database olusturuldu migration

--- ECommerce/ECommerce.DataAccess/Concrete/EfCore/ShopContext.cs
+++ ECommerce/ECommerce.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -13,6 +13,11 @@ namespace ECommerce.DataAccess.Concrete.EfCore
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ShopDb;integrated security=true;");
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductCategory>().HasKey(x => new { x.CategoryId, x.ProductId });
+        }
+
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
