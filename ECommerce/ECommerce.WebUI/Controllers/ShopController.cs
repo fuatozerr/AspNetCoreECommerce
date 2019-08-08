c254f6ae858264d5d0c8610d946c8815eb515b3a
fuat özer <fuatozerr23@gmail.com>
2019-08-08 16:18:35 +0300

    display product

--- ECommerce/ECommerce.WebUI/Controllers/ShopController.cs
+++ ECommerce/ECommerce.WebUI/Controllers/ShopController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Business.Abstract;
+using ECommerce.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.WebUI.Controllers
 {
     public class ShopController : Controller
     {
+        private IProductService _productService;
+
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        
+        public IActionResult List()
+        {
+            return View(new ProductListModel()
+            {
+                Products = _productService.GetAll()
+
+            });
         }
     }
 }
