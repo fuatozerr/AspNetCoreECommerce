7c6e53b80ba9186ef41cbd9100144429d6e68b56
fuat özer <fuatozerr23@gmail.com>
2019-08-11 15:18:59 +0300

    crete product

--- ECommerce/ECommerce.Entities/Product.cs
+++ ECommerce/ECommerce.Entities/Product.cs
@@ -5,13 +5,13 @@ using System.Text;
 namespace ECommerce.Entities
 {
     public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
-        public string Description { get; set; }
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string ImageUrl { get; set; }
+            public string Description { get; set; }
 
-        public decimal Price { get; set; }
+            public decimal Price { get; set; }
         public List<ProductCategory> ProductCategories { get; set; }
     }
 }
--- ECommerce/ECommerce.WebUI/Controllers/AdminController.cs
+++ ECommerce/ECommerce.WebUI/Controllers/AdminController.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Business.Abstract;
+using ECommerce.Entities;
+using ECommerce.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel model)
+        {
+            var entity = new Product
+            {
+                Name = model.Name,  
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                Price = model.Price
+            };
+
+            _productService.Create(entity);
+            return Redirect("Index");
+
         }
     }
 }
