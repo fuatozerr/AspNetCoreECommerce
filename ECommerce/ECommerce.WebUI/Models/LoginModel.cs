dd99115e3d4eb6f0a581800c1e0e0bc101d0c7c3
fuat özer <fuatozerr23@gmail.com>
2019-08-21 23:14:09 +0300

    login by username

---
+++ ECommerce/ECommerce.WebUI/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.WebUI.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Yanlıs kullanıcı adı")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
