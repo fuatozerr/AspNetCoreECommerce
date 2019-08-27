84520d7d787e1add92e7ac35922d8d1180900477
fuat özer <fuatozerr23@gmail.com>
2019-08-27 13:51:39 +0300

    cart view

---
+++ ECommerce/ECommerce.WebUI/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(Cart model)
+        {
+            return View();
+        }
+    }
+}
